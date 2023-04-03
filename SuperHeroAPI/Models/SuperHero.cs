ea48f21e9b6de69afb3d660686bb9ebbe12fc1c2
LPiotr1 <piotr.lebelt1@gmail.com>
2023-04-04 00:36:47 +0200

    Dodane odpowiedniki nazw w bazie! Zabanglało. :)

--- SuperHeroAPI/Models/SuperHero.cs
+++ SuperHeroAPI/Models/SuperHero.cs
@@ -8,8 +8,16 @@ public class SuperHero
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = String.Empty;
+    
+    [BsonElement("name")]
     public string Name { get; set; } = String.Empty;
+    
+    [BsonElement("firstName")]
     public string FirstName { get; set; } = String.Empty;
+    
+    [BsonElement("lastName")]
     public string LastName { get; set; } = String.Empty;
+    
+    [BsonElement("place")]
     public string Place { get; set; } = String.Empty;
 }
